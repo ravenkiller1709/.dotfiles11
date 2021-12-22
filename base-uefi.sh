995c8057189481ce1df468a601d72c7f395379e1
Kim Kruse <kim@kimkruse.xyz>
2021-12-22 10:17:52 +0100

    corrected script

--- base-uefi.sh
+++ base-uefi.sh
@@ -15,7 +15,7 @@ echo root:password | chpasswd
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
 
-pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font xf86-vide-intel
+pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font xf86-vide-intel xorg xorg-apps xorg-xinit xorg-server
 
 # pacman -S --noconfirm xf86-video-amdgpu
 # pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
@@ -39,7 +39,7 @@ useradd -m kim
 echo kim:password | chpasswd
 usermod -aG libvirt kim
 
-echo "kim ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/ermanno
+echo "kim ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/kim
 
 
 printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
